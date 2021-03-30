1c2a184bef316978faa5f7c4d78dd24ed0c236db
asledz <anita.gaja.sledz@gmail.com>
2021-03-30 18:30:57 +0200

    Final solution

--- src/Graph.hs
+++ src/Graph.hs
@@ -1,21 +1,23 @@
 module Graph where
-import Set(Set)
-import qualified Set as Set
+
 import Data.List (sort)
+import Set (Set)
+import qualified Set as Set
 
 class Graph g where
-  empty   :: g a
-  vertex  :: a -> g a
-  union   :: g a -> g a -> g a
+  empty :: g a
+  vertex :: a -> g a
+  union :: g a -> g a -> g a
   connect :: g a -> g a -> g a
 
-data Relation a = Relation { domain :: Set a, relation :: Set (a, a) }
-    deriving (Eq, Show)
+data Relation a = Relation {domain :: Set a, relation :: Set (a, a)}
+  deriving (Eq, Show)
 
-data Basic a = Empty
-             | Vertex a
-             | Union (Basic a) (Basic a)
-             | Connect (Basic a) (Basic a)
+data Basic a
+  = Empty
+  | Vertex a
+  | Union (Basic a) (Basic a)
+  | Connect (Basic a) (Basic a)
 
 createUnionDomain :: Set a -> Set a -> Set a
 createUnionDomain = Set.union
@@ -27,74 +29,66 @@ createConnectDomain :: Set a -> Set a -> Set a
 createConnectDomain = createUnionDomain
 
 createConnectRelation :: Relation a -> Relation a -> Set (a, a)
-createConnectRelation graph1 graph2 = 
-  let pair1 = createPairList (Set.toList(domain graph1)) (Set.toList(domain graph2)) in
-  -- let pair2 = createPairList (Set.toList(domain graph2)) (Set.toList(domain graph1)) in
-  let new_relations = Set.fromList pair1 in 
-  let old_relations = Set.union (relation graph1) (relation graph2) in
-  Set.union new_relations old_relations
-
-createPairs :: a -> [a] ->[(a, a)]
-createPairs e = map (\ a -> (a, e))
-
-createPairList :: [a] -> [a] -> [(a,a)]
-createPairList list = concatMap (\ a -> createPairs a list)
+createConnectRelation graph1 graph2 =
+  let pair1 = [(a, b) | a <- Set.toList (domain graph1), b <- (Set.toList (domain graph2))]
+   in let new_relations = Set.fromList pair1
+       in let old_relations = createUnionRelation graph1 graph2
+           in Set.union new_relations old_relations
 
 instance Graph Relation where
-  empty = Relation {domain = Set.empty, relation = Set.empty} 
-  vertex a = Relation {domain = Set.singleton a, relation = Set.empty} 
-  union graph1 graph2 = 
-    Relation {
-      domain = Set.union (domain graph1) (domain graph2), 
-      relation = Set.union (relation graph1) (relation graph2)
-    } 
-  connect graph1 graph2 = 
-    Relation {
-      domain = Set.union (domain graph1) (domain graph2),
-      relation = createConnectRelation graph1 graph2
-    } 
-                
+  empty = Relation {domain = Set.empty, relation = Set.empty}
+  vertex a = Relation {domain = Set.singleton a, relation = Set.empty}
+  union graph1 graph2 =
+    Relation
+      { domain = createUnionDomain (domain graph1) (domain graph2),
+        relation = createUnionRelation graph1 graph2
+      }
+  connect graph1 graph2 =
+    Relation
+      { domain = createConnectDomain (domain graph1) (domain graph2),
+        relation = createConnectRelation graph1 graph2
+      }
+
 instance (Ord a, Num a) => Num (Relation a) where
-  fromInteger     = vertex . fromInteger
-  (+)             = union
-  (*)             = connect
-  signum          = const empty 
-  abs             = id 
-  negate          = id
+  fromInteger = vertex . fromInteger
+  (+) = union
+  (*) = connect
+  signum = const empty
+  abs = id
+  negate = id
 
 instance Graph Basic where
-    empty = Empty 
-    vertex a = Vertex a 
-    union a b = Union a b 
-    connect a b = Connect a b 
+  empty = Empty
+  vertex a = Vertex a
+  union a b = Union a b
+  connect a b = Connect a b
 
 instance Ord a => Eq (Basic a) where
-    graph1 == graph2 =
-        let domain1 = domain (fromBasic graph1) in
-        let domain2 = domain (fromBasic graph2) in
-        let relations1 = relation (fromBasic graph1) in
-        let relations2 = relation (fromBasic graph2) in
-        domain1 == domain2 && relations1 == relations2
+  graph1 == graph2 =
+    let domain1 = domain (fromBasic graph1)
+     in let domain2 = domain (fromBasic graph2)
+         in let relations1 = relation (fromBasic graph1)
+             in let relations2 = relation (fromBasic graph2)
+                 in domain1 == domain2 && relations1 == relations2
 
 instance (Ord a, Num a) => Num (Basic a) where
-    fromInteger = vertex . fromInteger
-    (+)         = union
-    (*)         = connect
-    signum      = const empty
-    abs         = id
-    negate      = id
+  fromInteger = vertex . fromInteger
+  (+) = union
+  (*) = connect
+  signum = const empty
+  abs = id
+  negate = id
 
 instance Semigroup (Basic a) where
   (<>) = union
 
 instance Monoid (Basic a) where
-    mempty = Empty
-    mappend = (<>)
-    mconcat = foldr mappend mempty
+  mempty = Empty
+  mappend = (<>)
+  mconcat = foldr mappend mempty
 
 fromBasic :: Graph g => Basic a -> g a
-
-fromBasic Empty = empty 
+fromBasic Empty = empty
 fromBasic (Vertex a) = vertex a
 fromBasic (Union g1 g2) = union (fromBasic g1) (fromBasic g2)
 fromBasic (Connect g1 g2) = connect (fromBasic g1) (fromBasic g2)
@@ -102,77 +96,74 @@ fromBasic (Connect g1 g2) = connect (fromBasic g1) (fromBasic g2)
 getAllVertex :: (Ord a, Show a) => Basic a -> [a]
 getAllVertex graph = Set.toAscList (domain (fromBasic graph))
 
-convertPairToList :: [(a,a)] -> [a]
-convertPairToList = concatMap (\(x,y) -> [x,y])
+convertPairToList :: [(a, a)] -> [a]
+convertPairToList = concatMap (\(x, y) -> [x, y])
 
 getConnectedVertex :: (Ord a, Show a) => Basic a -> [a]
-getConnectedVertex graph = Set.deleteDuplicates (sort (convertPairToList (Set.toAscList(relation (fromBasic graph)))))
+getConnectedVertex graph = Set.deleteDuplicates (sort (convertPairToList (Set.toAscList (relation (fromBasic graph)))))
 
 getSingleVertex :: (Ord a, Show a) => Basic a -> [a]
-getSingleVertex graph = 
-  let connectedVertex = getConnectedVertex graph in
-  let allVertex = getAllVertex graph in
-  filter (`notElem` connectedVertex) allVertex
+getSingleVertex graph =
+  let connectedVertex = getConnectedVertex graph
+   in let allVertex = getAllVertex graph
+       in filter (`notElem` connectedVertex) allVertex
 
-getAllRelations :: (Ord a, Show a) => Basic a -> [(a,a)]
+getAllRelations :: (Ord a, Show a) => Basic a -> [(a, a)]
 getAllRelations graph = Set.toAscList (relation (fromBasic graph))
 
-instance (Ord a, Show a) => Show (Basic a) where 
-  show graph = 
-    let edges = getAllRelations graph in
-    let verticles = getSingleVertex graph in
-    "edges " ++ show edges ++ " + verticles " ++ show verticles
+instance (Ord a, Show a) => Show (Basic a) where
+  show graph =
+    let edges = getAllRelations graph
+     in let verticles = getSingleVertex graph
+         in "edges " ++ show edges ++ " + vertices " ++ show verticles
 
 -- | Example graph
 -- >>> example34
 -- edges [(1,2),(2,3),(2,4),(3,5),(4,5)] + vertices [17]
 
 example34 :: Basic Int
-example34 = 1*2 + 2*(3+4) + (3+4)*5 + 17
+example34 = 1 * 2 + 2 * (3 + 4) + (3 + 4) * 5 + 17
 
-
-createEdgesString :: (Ord a, Show a) => [(a, a)] -> [Char] 
+createEdgesString :: (Ord a, Show a) => [(a, a)] -> [Char]
 createEdgesString list = reverse (createEdgesString' list [])
 
 insertCharAtBeggining :: [Char] -> [Char] -> [Char]
 insertCharAtBeggining list acc = foldl (flip (:)) acc list
 
-createEdgesString' :: (Ord a, Show a) => [(a, a)] -> [Char] -> [Char] 
+createEdgesString' :: (Ord a, Show a) => [(a, a)] -> [Char] -> [Char]
 createEdgesString' [] acc = acc
-createEdgesString' ((a, b):t) acc = 
-  let relation = show a ++ "->" ++ show b++ ";" in
-  let new_acc = insertCharAtBeggining relation acc in
-  createEdgesString' t new_acc
+createEdgesString' ((a, b) : t) acc =
+  let relation = show a ++ "->" ++ show b ++ ";"
+   in let new_acc = insertCharAtBeggining relation acc
+       in createEdgesString' t new_acc
 
-
-createVerticlesString :: (Ord a, Show a) => [a] -> [Char] 
+createVerticlesString :: (Ord a, Show a) => [a] -> [Char]
 createVerticlesString list = reverse (createVerticlesString' list [])
 
-createVerticlesString' :: (Ord a, Show a) => [a] -> [Char] -> [Char] 
+createVerticlesString' :: (Ord a, Show a) => [a] -> [Char] -> [Char]
 createVerticlesString' [] acc = acc
-createVerticlesString' (h:t) acc = 
-  let verticle = show h ++ ";" in
-  let new_acc = insertCharAtBeggining verticle acc in
-    createVerticlesString' t new_acc
-
+createVerticlesString' (h : t) acc =
+  let verticle = show h ++ ";"
+   in let new_acc = insertCharAtBeggining verticle acc
+       in createVerticlesString' t new_acc
 
 todot :: (Ord a, Show a) => Basic a -> String
-todot graph = 
-  let edges = getAllRelations graph in
-  let verticles = getSingleVertex graph in
-  let edges_string = createEdgesString edges in
-  let verticles_string = createVerticlesString verticles in
-  "digraph {" ++ edges_string ++ verticles_string ++ "}"
+todot graph =
+  let edges = getAllRelations graph
+   in let verticles = getSingleVertex graph
+       in let edges_string = createEdgesString edges
+           in let verticles_string = createVerticlesString verticles
+               in "digraph {" ++ edges_string ++ verticles_string ++ "}"
 
-instance Functor Basic where 
+instance Functor Basic where
   fmap f Empty = Empty
   fmap f (Vertex a) = Vertex (f a)
   fmap f (Union g1 g2) = Union (fmap f g1) (fmap f g2)
-  fmap f (Connect g1 g2) = Connect (fmap f g1) (fmap f g2) 
+  fmap f (Connect g1 g2) = Connect (fmap f g1) (fmap f g2)
 
--- -- | Merge vertices
--- -- >>> mergeV 3 4 34 example34
--- -- edges [(1,2),(2,34),(34,5)] + vertices [17]
+-- | Merge vertices
+-- >>> mergeV 3 4 34 example34
+-- edges [(1,2),(2,34),(34,5)] + vertices [17]
 
 mergeV :: Eq a => a -> a -> a -> Basic a -> Basic a
 mergeV v w destination (Vertex a) = if v == a || w == a then Vertex destination else Vertex a
@@ -180,21 +171,41 @@ mergeV v w destination (Connect g1 g2) = Connect (mergeV v w destination g1) (me
 mergeV v w destination (Union g1 g2) = Union (mergeV v w destination g1) (mergeV v w destination g2)
 mergeV _ _ _ Empty = Empty
 
--- instance Applicative Basic where
+-- http://learnyouahaskell.com/functors-applicative-functors-and-monoids
 
+instance Applicative Basic where
+  Empty <*> f = Empty
+  Vertex g <*> f = fmap g f
+  Connect g1 g2 <*> f =
+    let app1 = g1 <*> f
+     in let app2 = g2 <*> f
+         in connect app1 app2
+  Union g1 g2 <*> f =
+    let app1 = g1 <*> f
+     in let app2 = g2 <*> f
+         in union app1 app2
+  pure = Vertex
 
--- instance Monad Basic where
---          Empty >>= _ = Empty
---          Vertex v >>= f = f v
---          Union a b >>= f = Union (a >>= f) (b >>= f)
---          Connect a b >>= f = Connect (a >>= f) (b >>= f)
---          return v = Vertex v
+instance Monad Basic where
+  Empty >>= f = Empty
+  Vertex v >>= f = f v
+  Union g1 g2 >>= f =
+    let mon1 = g1 >>= f
+     in let mon2 = g2 >>= f
+         in union mon1 mon2
+  Connect g1 g2 >>= f =
+    let mon1 = g1 >>= f
+     in let mon2 = g2 >>= f
+         in connect mon1 mon2
+  return v = Vertex v
 
--- -- | Split Vertex
--- -- >>> splitV 34 3 4 (mergeV 3 4 34 example34)
--- -- edges [(1,2),(2,3),(2,4),(3,5),(4,5)] + vertices [17]
+-- | Split Vertex
+-- >>> splitV 34 3 4 (mergeV 3 4 34 example34)
+-- edges [(1,2),(2,3),(2,4),(3,5),(4,5)] + vertices [17]
 
--- splitV :: Eq a => a -> a -> a -> Basic a -> Basic a
--- splitV v des1 des2 Empty = Empty
--- splitV v des1 des2 (Vertex a) = if a == v then Union (Vertex des1) (Vertex des2) else Vertex a
+splitFun :: Eq a => a -> a -> a -> a -> Basic a
+splitFun toSplit a b v =
+  if toSplit == v then Union (Vertex a) (Vertex b) else return v
 
+splitV :: Eq a => a -> a -> a -> Basic a -> Basic a
+splitV v a b graph = graph >>= (splitFun v a b)
--- src/Set.hs
+++ src/Set.hs
@@ -6,19 +6,20 @@ import Data.List (sort)
 import Data.Semigroup
 import Data.Monoid
 
-data Set a = Empty
-           | Singleton a
-           | Union (Set a) (Set a)
+data Set a
+  = Empty
+  | Singleton a
+  | Union (Set a) (Set a)
 
 empty :: Set a
 empty = Empty
 
 null :: Set a -> Bool
-null Empty = True 
+null Empty = True
 null _ = False
 
 member :: Eq a => a -> Set a -> Bool
-member _ Empty = False 
+member _ Empty = False
 member a (Singleton b) = a == b
 member a (Union x y) = member a x || member a y
 
@@ -30,35 +31,35 @@ fromList l = fromList' l Empty
 
 fromList' :: [a] -> Set a -> Set a
 fromList' [] set = set
-fromList' (h:t) set = fromList' t (insert h set)
+fromList' (h : t) set = fromList' t (insert h set)
 
 toList :: Set a -> [a]
 toList set = toList' set []
 
 toList' :: Set a -> [a] -> [a]
 toList' Empty list = list
-toList' (Singleton a) list = a:list
-toList' (Union x y) list = toList' y (toList' x list)
+toList' (Singleton a) list = a : list
+toList' (Union x y) list = toList' x (toList' y list)
 
 toAscList :: Ord a => Set a -> [a]
 toAscList set = reverse (deleteDuplicates (sort (toList set)))
 
 deleteDuplicates :: Ord a => [a] -> [a]
-deleteDuplicates (h:t) = deleteDuplicates' h t [h]
+deleteDuplicates (h : t) = deleteDuplicates' h t [h]
 deleteDuplicates [] = []
 
 deleteDuplicates' :: Ord a => a -> [a] -> [a] -> [a]
 deleteDuplicates' a [] acc = acc
-deleteDuplicates' a (h:t) acc =
-    if h == a then deleteDuplicates' a t acc 
-    else deleteDuplicates' h t (h:acc)
+deleteDuplicates' a (h : t) acc =
+  if h == a
+    then deleteDuplicates' a t acc
+    else deleteDuplicates' h t (h : acc)
 
 elems :: Set a -> [a]
 elems = toList
 
 union :: Set a -> Set a -> Set a
 -- union with an empty set
-union Empty Empty = Empty
 union set Empty = set
 union Empty set = set
 -- union with singleton
@@ -70,32 +71,32 @@ union s1 s2 = Union s1 s2
 insert :: a -> Set a -> Set a
 insert a Empty = Singleton a
 insert a (Singleton b) = Union (Singleton a) (Singleton b)
-insert a (Union x y) = Union x (insert a y) 
+insert a g = Union g (Singleton a)
 
 instance Ord a => Eq (Set a) where
-    -- Empty set
-    Empty == Empty = True 
-    Empty == _ = False 
-    _ == Empty = False 
-    -- Singleton set
-    Singleton a == Singleton b = a == b
-    -- Union set
-    set1 == set2 = toAscList set1 == toAscList set2
+  -- Empty set
+  Empty == Empty = True
+  Empty == _ = False
+  _ == Empty = False
+  -- Singleton set
+  Singleton a == Singleton b = a == b
+  -- Union set
+  set1 == set2 = toAscList set1 == toAscList set2
 
 instance Semigroup (Set a) where
-    s1 <> s2 = union s1 s2
+  s1 <> s2 = union s1 s2
 
 instance Monoid (Set a) where
-    mempty = empty
-    mappend = (<>)
-    mconcat = foldr mappend mempty
+  mempty = empty
+  mappend = (<>)
+  mconcat = foldr mappend mempty
 
 instance Show a => Show (Set a) where
-    show Empty = "[]"
-    show (Singleton a) = show a
-    show (Union s1 s2) =  (show s1) ++ "," ++ (show s2)
+  show Empty = "[]"
+  show (Singleton a) = show a
+  show (Union s1 s2) = (show s1) ++ "," ++ (show s2)
 
 instance Functor Set where
-    fmap f Empty = Empty
-    fmap f (Singleton a) = Singleton (f a)
-    fmap f (Union s1 s2) = Union (fmap f s1) (fmap f s2)
+  fmap f Empty = Empty
+  fmap f (Singleton a) = Singleton (f a)
+  fmap f (Union s1 s2) = Union (fmap f s1) (fmap f s2)
